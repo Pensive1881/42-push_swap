01d9bd9b7c9186fcd5ff06fb03ef0ef966af9cad
Alexander Casper <acasper@student.42berlin.de>
2025-11-24 16:57:15 +0100

    began push_swap.h & created src folder

--- push_swap.h
+++ push_swap.h
@@ -6,8 +6,16 @@
 /*   By: acasper <acasper@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 16:15:17 by acasper           #+#    #+#             */
-/*   Updated: 2025/11/24 16:15:19 by acasper          ###   ########.fr       */
+/*   Updated: 2025/11/24 16:56:40 by acasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
 
 
+
+#endif
